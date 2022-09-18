08e14bce96612ab8c19b5e91e10468d2dde6de7a
Gokul <gokuljune2003@gmail.com>
2022-09-18 10:30:03 +0530

    increased width

--- src/component/Billingdisplay.jsx
+++ src/component/Billingdisplay.jsx
@@ -5,7 +5,7 @@ import './billing.css';
 
 const style ={
     display:{
-        width:'350px',
+        width:'370px',
         height:'8vh',
         borderRadius:'10px',
         border:'1px solid grey',
