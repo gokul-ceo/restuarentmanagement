8d694602a9afc62a3c51ef6a990871dcd8bc80de
gokulg001 <gokuljune2003@gmail.com>
2023-01-15 14:03:44 +0530

    made some changes

--- src/component/Auth/login.jsx
+++ src/component/Auth/login.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import hello from './hello.gif'
+var date = new Date()
+var year =  date.getFullYear()
 function Loginpage(){
     var mt = window.innerHeight/6
     return <>
     <div style={{'width':'300px','marginTop':`${mt}px`}} className="container-sm align-center shadow shadow-lg">
-    <img alt="alt" style={{'width':'200px','height':'200px','marginLeft':'15%'}} src={hello}></img>
+    <img alt="alt" style={{'width':'150px','height':'150px','marginLeft':'20%'}} src={hello}></img>
         <div className=" container-sm my-1 text-center ">
-            <h3 className="my-1">Login</h3>
+            <h5 className="my-2">Login to continue..</h5>
         <div class="form-floating mb-3  ">
   <input type="text" class="form-control" id="floatingInput" placeholder="Name"/>
   <label for="floatingInput">Name</label>
@@ -19,8 +21,13 @@ function Loginpage(){
 
 </div>
 </div>
-<button className="btn mb-5 btn-primary">Submit</button>
-
+<button className="btn mb-3 my-2 btn-primary">Submit</button>
+<div>
+    <span  style={{'fontSize':'12px'}} className="mx-1">Terms & condition </span>
+    <span style={{'fontSize':'12px'}} className=""> | </span>
+    <span style={{'fontSize':'12px'}} className="mx-1">Privacy policy</span>
+</div>
+<span style={{'margin':'0 auto','fontSize':'12px'}}> ©️copyright {year}</span>
         </div>
     </div>
     </>
